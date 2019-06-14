a42cca4c9331ce0880d16046b526d34eaa40247e
Mile Stojanovic <mile.stojanovic@c4media.com>
2019-06-14 11:37:46 +0200

    new page

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+(function() {
+  console.log('This is a lassibrary. It should be da first file');
+})();
+(function() {
+  console.log('This is aasdasdnother library. It should be the second file');
+})();
+console.log('This is da Main.js file. It should assdsd the 3rd and final fileo');
