1b0c24eb3da8e1107ddf005c31c9b433f6650646
Mile Stojanovic <mile.stojanovic@c4media.com>
2019-06-14 13:58:21 +0200

    New changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,9 @@
-(function() {
-  console.log('This is a lassibrary. It should be da first file');
-})();
-(function() {
-  console.log('This is aasdasdnother library. It should be the second file');
-})();
-console.log('This is da Main.js file. It should assdsd the 3rd and final fileo');
+
+function mobileNavigation() {
+  var x = document.getElementById("mobileNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
