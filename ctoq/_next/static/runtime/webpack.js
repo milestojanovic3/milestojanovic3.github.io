f623ebe3a224597d080e8ba89a4350ad6ce8311f
Mile Stojanovic <mile.stojanovic@c4media.com>
2020-06-04 09:56:42 +0200

    new images

--- ctoq/_next/static/runtime/webpack.js
+++ ctoq/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "578508b0d9dbe0c43adc";
+/******/ 	var hotCurrentHash = "1f347347f1b36ec78a63";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ ctoq/static/js/main.js
@@ -0,0 +1,10 @@
+// $("#close-lightbox-1").on("click", function () {
+//   var reSrc = $("#video-1").attr("src");
+//   $("#video-1").attr("src", reSrc);
+// });
+
+
+// document.getElementById("close-lightbox-1").onclick = function(){
+//     var reSrc = document.getElementById('video-1').attr("src");
+//     document.getElementById("#video-1").attr("src", reSrc);
+// };
