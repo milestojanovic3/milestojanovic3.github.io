bd0e6e465deba93184b459ff38429c275258a7d9
Mile Stojanovic <mile.stojanovic@c4media.com>
2020-06-05 12:30:49 +0200

    new video

--- ctoq/_next/static/runtime/webpack.js
+++ ctoq/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "38875367c179dbee854a";
+/******/ 	var hotCurrentHash = "505b98d8f96cab905a7e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
