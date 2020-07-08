cd94a485decc3e963f5531b9ad93e784947261a4
Mile Stojanovic <mile.stojanovic@c4media.com>
2020-07-08 10:57:40 +0200

    event listing

--- events-list/_next/static/runtime/webpack.js
+++ events-list/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0b7cd233c544357ad1f0";
+/******/ 	var hotCurrentHash = "47542ad08e9e3828f7da";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
