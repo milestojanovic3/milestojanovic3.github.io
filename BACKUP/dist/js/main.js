140ff305d297119284bff02935b73990305cd2c6
Mile Stojanovic <mile.stojanovic@c4media.com>
2020-07-27 00:17:25 +0200

    remove old files

--- BACKUP/dist/js/main.js
+++
@@ -1,9 +0,0 @@
-
-function mobileNavigation() {
-  var x = document.getElementById("mobileNav");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
